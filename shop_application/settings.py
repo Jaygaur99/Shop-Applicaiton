daaef0b8eb5cf497e6ac1474725a991fad7d95b0
Jay Kumar Gaur <jaygaur99@gmail.com>
2021-08-17 08:49:03 +0530

    t

--- shop_application/settings.py
+++ shop_application/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
-# 'django-insecure-qo1%nqwov6s2i410q3tllai+91@$sxi-#tx#p&3q9n9e020tuu'
+SECRET_KEY = 'django-insecure-qo1%nqwov6s2i410q3tllai+91@$sxi-#tx#p&3q9n9e020tuu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -125,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
